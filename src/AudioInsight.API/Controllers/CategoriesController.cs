3b0345417c1baa3ae6458c8a559fd5da82ec5e14
Serhii Dmytrenko <dmmytrenko@gmail.com>
2024-09-27 12:20:20 +0300

    init commit

---
+++ src/AudioInsight.API/Controllers/CategoriesController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AudioInsight.API.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<CategoriesController> _logger;
+
+    public CategoriesController(ILogger<CategoriesController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
